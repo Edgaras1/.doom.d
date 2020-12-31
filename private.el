55880424b1f43a5d5bf8c07315cb257def57f5d9
Edgaras1 <edgaras.kaz@gmail.com>
2021-01-01 00:49:52 +0200

    Create private.el

---
+++ private.el
@@ -0,0 +1,7 @@
+(setq user-full-name "John Doe"
+      user-mail-address "email@example.com"
+      org-directory "~/org"
+      org-agenda-files (list
+                        "~/org/tickler.org"
+                        "~/org/projects.org"
+                        "~/org/inbox.org"))
