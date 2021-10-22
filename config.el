7a5c7a89e05cc52b9d696ac7c0deff5f049e58e6
Edgaras <edgaras.kaz@gmail.com>
2021-10-22 12:27:04 +0300

    added org-roam-ui

--- config.el
+++ config.el
@@ -247,6 +247,21 @@
      ("i" "Inbox" entry  (file "~/Dropbox/ORG/inbox.org") ,(concat "* TODO %?\n" "/Entered on/ %U")))
                 ))
 
+(use-package! websocket
+    :after org-roam)
+
+(use-package! org-roam-ui
+    :after org-roam ;; or :after org
+;;         normally we'd recommend hooking orui after org-roam, but since org-roam does not have
+;;         a hookable mode anymore, you're advised to pick something yourself
+;;         if you don't care about startup time, use
+;;  :hook (after-init . org-roam-ui-mode)
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
 ;hide markers
 (setq org-hide-emphasis-markers t)
 ;https://github.com/awth13/org-appear
--- packages.el
+++ packages.el
@@ -6,3 +6,8 @@
 (package! yequake)
 (package! org-web-tools)
 (package! dashboard)
+
+;Org-Roam-Ui
+(unpin! org-roam)
+(package! websocket)
+(package! org-roam-ui :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
